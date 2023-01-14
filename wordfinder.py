5b08f02a56e550e9e900a4a834c13c1e99aa692b
megoshmego <108840586+megoshmego@users.noreply.github.com>
2023-01-14 12:56:06 -0500

    troubleshooting file

--- wordfinder.py
+++ wordfinder.py
@@ -7,14 +7,15 @@ class WordFinder:
     def __init__(self, path):
         """Read dictionary and reports number of words"""
     
-        words = open(words.txt)
-        self.words = self.parse(words) 
+        dict_file = open(path)
+        self.words = self.parse("megoshmego/bootcamp_python/words.txt", "r") 
         print(f"{len(self.words)} words read")
     
-    def parse(self, words):  
+    def parse(self, dict_file):  
         """Parse dict_file -> list of words"""
-        
-        return [w.strip() for w in words]
+        return [w.strip() for w in dict_file]
     
     def random(self):
+        """Return random word."""
+        
         return random.choice(self.words)
