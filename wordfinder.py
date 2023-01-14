128a7c30672ff63693452ba7af09113265f7510b
megoshmego <108840586+megoshmego@users.noreply.github.com>
2023-01-13 20:54:29 -0500

    first attempt

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,19 @@
 """Word Finder: finds random words from a dictionary."""
 
+import random
 
 class WordFinder:
-    ...
+    def __init__(self, words.txt):
+        """Read dictionary and reports number of words"""
+        
+        dict_file = open(words.txt)
+        self.words = self.parse(words) 
+        print(f"{len(self.words)} words read")
+    
+    def parse(self, words):  
+        """Parse dict_file -> list of words"""
+        
+        return [w.strip() for w in words]
+    
+    def random(self):
+        return random.choice(self.words)
