9142d264e5f1b85d86c1def9fc549c8a165e4d8d
megoshmego <108840586+megoshmego@users.noreply.github.com>
2023-01-15 05:59:54 -0500

    pre-format to class

--- wordfinder.py
+++ wordfinder.py
@@ -10,14 +10,11 @@ def get_list_of_words():
     )
 
     string_of_words = response.content.decode('utf-8')
-
     list_of_words = string_of_words.splitlines()
-    print(list_of_words)
-
     return list_of_words
 
-
 words = get_list_of_words()
-
+    
 random_word = random.choice(words)
+
 print(random_word)
